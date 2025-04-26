2e843aae155831becedccd7635acf398c77b0695
jagadeeshvarma927 <jagadeeshvarma927@gmail.com>
2025-04-26 21:21:45 +0530

    new files added requirements is added setup py is updated

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,14 @@
+from langchain_astradb import AstraDBVectorStore
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from data_ingestion.data_tranform import data_converter
+
+class ingest_data:
+    def __init__(self):
+        pass
+
+    def data_ingestion(self):
+        pass
+
+    
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(name="e-commerce-bot",version="0.0.1",
+     author="jagadeesh",
+     author_email="jagadeeshvarma928@gmail.com",
+     packages=find_packages(),
+    install_requires=['langchain-astradb','langchain'])
